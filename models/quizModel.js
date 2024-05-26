4fa9e3a8d33efb29565518793483d764f62bc896
GiftChrisJava <chrisjava77@gmail.com>
2024-05-26 13:58:50 +0200

    creating test models

--- models/quizModel.js
+++ models/quizModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
     questionText: { type: String, required: true },
+    allowsMultiple: { type: Boolean, default: true },
     options: [{ type: String, required: true }],
     correctAnswer: { type: String, required: true }
 });
